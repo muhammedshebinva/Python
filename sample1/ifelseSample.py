8b0e4a2f3f403dfe9cade7c2321ff86bea219e46
muhammedshebinva <muhammedshebinva@gmail.com>
2021-05-27 12:32:42 +0530

    list and else if

---
+++ sample1/ifelseSample.py
@@ -0,0 +1,11 @@
+number = int(input("enter a Number"))
+
+if number > 0:
+    print(str(number) + "is positive number")
+elif number == 0:
+    print("entered number is zero")
+else:
+    print(str(number) + "is negative number")
+
+
+
---
+++ sample1/listSample.py
@@ -0,0 +1,6 @@
+names = ["shebin", "adhil", "basheer", "Shemi"]
+print(names)
+print(names[1])
+
+names = names + ["unknown1", "unknown2"]
+print(names)
